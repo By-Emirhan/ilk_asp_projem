bc9a115471e99890998e119cbf9e4531affc4fbc
By-Emirhan <doganlardan190@gmail.com>
2025-04-13 16:24:19 +0300

    7.Ders

--- ilk_asp_projem/Controllers/HomeController.cs
+++ ilk_asp_projem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ilk_asp_projem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ilk_asp_projem.Controllers
 {
@@ -7,6 +8,20 @@ namespace ilk_asp_projem.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            var book = new List<kitap>()
+            {
+                new kitap() {ID=1,bookName="Araba Sevdası",bookWriter="Recaizade Mahmut Ekrem" },
+                new kitap() {ID=2,bookName="Yaprak Dökümü",bookWriter="Reşat Nuri Gültekin" },
+                new kitap() {ID=3,bookName="Aşk-ı Memnu",bookWriter="Halit Ziya Uşaklıgil" }
+            };
+            return View(book);
         }
     }
 }
---
+++ ilk_asp_projem/Models/kitap.cs
@@ -0,0 +1,9 @@
+﻿namespace ilk_asp_projem.Models
+{
+    public class kitap
+    {
+        public int ID { get; set; }
+        public string bookName { get; set; }
+        public string bookWriter { get; set; }
+    }
+}
